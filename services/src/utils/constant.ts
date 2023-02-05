f553626003c36da39211838c8398ae1c25aeed47
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-02-05 23:00:16 +0900

    create: constant variables

---
+++ services/src/utils/constant.ts
@@ -0,0 +1,6 @@
+export const DATABASE_TYPE     = 'mysql';
+export const DATABASE_HOST     = 'localhost';
+export const DATABASE_PORT     = 3306;
+export const DATABASE_USERNAME = 'test';
+export const DATABASE_PASSEORD = 'test';
+export const DATABASE_DATABASE = 'test';
