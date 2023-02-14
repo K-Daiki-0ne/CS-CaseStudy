67afecc9bf04f9988fdffed9ce4c96ca5def39da
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-02-14 22:39:43 +0900

    add: database connection function

--- services/src/utils/constant.ts
+++ services/src/utils/constant.ts
@@ -1,6 +1,14 @@
-export const DATABASE_TYPE     = 'mysql';
-export const DATABASE_HOST     = 'localhost';
-export const DATABASE_PORT     = 3306;
-export const DATABASE_USERNAME = 'test';
-export const DATABASE_PASSEORD = 'test';
-export const DATABASE_DATABASE = 'test';
+import dotenv from 'dotenv';
+
+export const readEnviroment = () => {
+  if(process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+  }
+};
+
+export const DATABASE_TYPE: string     = process.env.DATABASE_TYPE as string;
+export const DATABASE_HOST: string     = process.env.DATABASE_HOST as string;
+export const DATABASE_PORT: string     = process.env.DATABASE_PORT as string;
+export const DATABASE_USERNAME: string = process.env.DATABASE_USERNAME as string;
+export const DATABASE_PASSEORD: string = process.env.DATABASE_PASSEORD as string;
+export const DATABASE_DATABASE: string = process.env.DATABASE_DATABASE as string;
