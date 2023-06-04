86a5a648fc2f9b268559d75b64f93b8c5b0668a3
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-06-04 15:38:19 +0900

    modified: header component & main page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,31 +1,35 @@
 import * as React from 'react';
 import { Layuot } from '../components/Layout';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Header } from '../components/Header/Header'
+import { Paper, Box } from '@mui/material'
+
 
 const Home = () => {
   return (
     <Layuot>
-      <Box
+      <Header title='CaseStudy' />
+      <Paper 
         sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          position: 'relative',
+          backgroundColor: 'grey.800',
+          color: '#fff',
+          mb: 4,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
         }}
       >
-        <Typography variant="h3" component="h1" gutterBottom>
-          Case Study
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          CaseStudyはあなたの学習をサポートします。
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          CaseStudyを使うことであなたの行動は可視化されます。
-        </Typography>
-
-      </Box>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            backgroundColor: 'rgba(0,0,0,.3)',
+          }}
+        />
+      </Paper>
     </Layuot>
   );
 }
