65f4fbcd3877bf445d4e46317082e16debeb3830
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-06-10 22:39:48 +0900

    modified: markdown rendering

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { Layuot } from '../components/Layout';
 import { Header } from '../components/Header/Header'
-import { Paper, Box, Grid, Typography } from '@mui/material'
-
+import { Markdown } from '../components/Markdown/Markdown';
+import { Paper, Box, Grid, Typography } from '@mui/material';
+import { homePage } from '../markdown/homePage';
 
 const Home = () => {
   return (
@@ -29,25 +30,28 @@ const Home = () => {
             backgroundColor: 'rgba(0,0,0,.3)',
           }}
         />
-          <Grid container>
-            <Grid item md={6}>
-              <Box
-                sx={{
-                  position: 'relative',
-                  p: { xs: 3, md: 6 },
-                  pr: { md: 0 },
-                }}
-              >
-                <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-                  aaa
-                </Typography>
-                <Typography variant="h5" color="inherit" paragraph>
-                  bbb
-                </Typography>
-              </Box>
-            </Grid>
+        <Grid container>
+          <Grid item md={6}>
+            <Box
+              sx={{
+                position: 'relative',
+                p: { xs: 3, md: 6 },
+                pr: { md: 0 },
+              }}
+            >
+              <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                aaa
+              </Typography>
+              <Typography variant="h5" color="inherit" paragraph>
+                bbb
+              </Typography>
+            </Box>
           </Grid>
-        </Paper>
+        </Grid>
+      </Paper>
+      <Markdown>
+        { homePage }
+      </Markdown>
     </Layuot>
   );
 }
