65cdde165b8acdc78c3d2a358ce86b6a2d341cf3
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-08-12 11:06:48 +0900

    add: menu drawer function

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,15 +1,50 @@
-import { FC } from 'react';
-import { Toolbar, Button, Typography, AppBar } from '@mui/material';
+import { FC, useState } from 'react';
+import { 
+  Toolbar,
+  Button,
+  Typography,
+  AppBar,
+  Drawer,
+  IconButton 
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
 type HeaderProps = {
   title: string
 }
 
 export const Header: FC<HeaderProps> = (props) => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <AppBar component='nav' >
       <Toolbar >
-        {/* <Button size="small">Subscribe</Button> */}
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="end"
+          onClick={() => setOpen(true)}
+          sx={{ ...(open && { display: 'none' }) }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          sx={{
+            width: 180,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: 180,
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <IconButton onClick={() => setOpen(false)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Drawer>
         <Typography
           component="h2"
           variant="h4"
@@ -20,7 +55,17 @@ export const Header: FC<HeaderProps> = (props) => {
         >
           {props.title}
         </Typography>
-        <Button variant="text" color='inherit'>
+        <Button 
+          variant="text" 
+          color='inherit'
+          sx={{
+            width: 180,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: 180,
+            },
+          }}
+        >
           Sign up
         </Button>
       </Toolbar>
