8f47ced4db6ff95ce006ca0d0328d3d0de7841fb
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-08-27 13:46:27 +0900

    modified: forgot password page design

--- web/src/pages/forgot-passwrod.tsx
+++ web/src/pages/forgot-passwrod.tsx
@@ -6,8 +6,11 @@ import {
   Alert,
   TextField,
   Button,
+  IconButton
 } from '@mui/material';
 import { Layuot } from '../components/Layout';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 const SuccessSendMail =(): JSX.Element => {
   return <Alert severity="success">mailが送信されました</Alert>
@@ -27,16 +30,26 @@ const ForgotPassword: NextPage = () => {
           width: 'auto',
         }}
       >
-        <Typography component="h1" variant="h5">
+        <ErrorOutlineIcon 
+          sx={{
+            color: 'primary.main',
+            width: '15%',
+            height: '15%'
+          }}
+        />
+        <Typography component="h1" variant="h5" sx={{ fontWeight: 'bold', mt: 1, mb: 1 }}>
           パスワード再設定
         </Typography>
-        <Box component="form" noValidate sx={{ mt: 3}}>
+        <Typography component='p' sx={{ mt: 1, mb: 1 }}>
+          ご入力いただいたメールアドレスに、パスワード再設定用のリンクをおくります。
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 , alignItems: 'center'}}>
           <TextField
             margin="normal"
             required
             fullWidth
             id="email"
-            label="Email Address"
+            label="メールアドレス"
             name="email"
             autoComplete="email"
             inputProps={{ maxLength: 50, pattern: "^[a-zA-Z0-9_]+$" }}
@@ -49,7 +62,22 @@ const ForgotPassword: NextPage = () => {
           >
             Sign Up
           </Button>
-        </Box>        
+          {/* <IconButton>
+            <ArrowBackIosIcon />
+
+          </IconButton> */}
+          <Button
+            variant="text"
+            color='inherit'
+            fullWidth
+            sx={{ mt:1, mb: 1 }}
+          >
+            <ArrowBackIosIcon />
+            <Typography>
+              Loginに戻る
+            </Typography>
+          </Button>
+        </Box>
       </Box>
     </Layuot>
   )
