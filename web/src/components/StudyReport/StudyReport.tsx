cc0a8b26a814eb2a7c4e07d195244a5df32952e1
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-01 01:04:24 +0900

    学習時間の合計時間（期間ごと）を表示するデザインを追加

--- web/src/components/StudyReport/StudyReport.tsx
+++ web/src/components/StudyReport/StudyReport.tsx
@@ -1,26 +1,73 @@
+import { FC } from 'react';
 import { Box, Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
+
+type StudyTimeboxProps = {
+  text: string,
+  heder?: boolean
+}
+
+const StudyTimebox: FC<StudyTimeboxProps> = ({ text, heder }) => {
+  let boxColor = '#FFFFFF';
+  let textBold = 'normal';
+  let textColor = '#000000';
+
+  if (heder) {
+    boxColor = '#556cd6';
+    textBold = 'bold';
+    textColor = 'white'
+  }
+
+  return (
+    <Box 
+      sx={{ 
+        border: '1px solid grey', 
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '70%',
+        backgroundColor: boxColor
+      }}>
+      <Typography 
+        align='center'
+        sx={{
+          color: textColor,
+          fontWeight: textBold,
+          fontSize: '0.83rem'
+        }}
+        component='p'
+      >
+        { text }
+      </Typography>
+    </Box>
+  )
+}
 
 export const StudyReport = () => {
   return (
     <Box>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 3, sm: 3, md: 3 }}>
+      <Box>
+        <Grid container spacing={0}>
           <Grid xs={4}>
-            <Typography>今日の学習時間</Typography>
+            <StudyTimebox text='今日の学習時間' heder/>
+            <StudyTimebox text='8時間' />
           </Grid>
           <Grid xs={4}>
-            <Typography>今週の学習時間</Typography>
+            <StudyTimebox text='今週の学習時間' heder/>
+            <StudyTimebox text='56時間' />
           </Grid>
           <Grid xs={4}>
-            <Typography>今月の学習時間</Typography>
+            <StudyTimebox text='今月の学習時間' heder/>
+            <StudyTimebox text='230時間' />
           </Grid>
         </Grid>
       </Box>
-      <Box sx={{ flexGrow: 1 }}>
+
+      <Box sx={{ flexGrow: 1, mt: 4 , border: '1px solid grey'}}>
         <Typography>Chartを設置する</Typography>
       </Box>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 2 }}>
+
+      <Grid container spacing={0}>
         <Grid xs={6}>
           <Typography>トータルの学習時間</Typography>
         </Grid>
