5da09c51b754f64f0d3ecb85f549a5c65c142564
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-05 01:24:09 +0900

    create: user post study page design

---
+++ web/src/pages/create/[userId].tsx
@@ -0,0 +1,93 @@
+import { useState, useEffect, ChangeEvent } from 'react';
+import { NextPage } from 'next';
+import dayjs, { Dayjs } from 'dayjs';
+import { Layuot } from '../../components/Layout';
+import { Box, Typography, Input, InputLabel, Select, MenuItem, TextField , Fab} from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { SelectChangeEvent } from '@mui/material/Select';
+import SaveIcon from '@mui/icons-material/Save';
+
+const Create: NextPage = () => {
+  const [date, setDate] = useState<Dayjs | null>(null);
+  const [time, setTime] = useState<string>('');
+  const [studyTag, setStudyTag] = useState<string>('');
+  const [study, setStudy] = useState<string>('');
+
+  useEffect(() => {
+    const today = dayjs()
+    setDate(today)
+  },[]);
+
+  const handleTagChange = (event: SelectChangeEvent) => {
+    event.preventDefault();
+    setStudyTag(event.target.value);
+  }
+
+  return (
+    <Layuot>
+      <Box sx={{ mt: 5 }}>
+        <Typography sx={{ textAlign: 'center' }}>学習内容を記録</Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <InputLabel sx={{ mt:5 }} shrink htmlFor="study-day-input" size='normal'>
+            学習日
+          </InputLabel>
+          <DateTimePicker 
+            value={date}
+            onChange={(value) => setDate(value)}
+            sx={{ width: '100%' }}
+            ampm={false}
+            views={['year', 'month', 'day', 'hours', 'minutes']}
+          />
+        </LocalizationProvider>
+        <InputLabel sx={{ mt:4 }} shrink htmlFor="study-day-input" size='normal'>
+          学習時間
+        </InputLabel>
+        <Input
+          required
+          value={time}
+          onChange={(value: ChangeEvent<HTMLInputElement>) => setTime(value.target.value)}
+          type='time'
+          fullWidth
+        />
+        <InputLabel sx={{ mt:4 }} shrink htmlFor="study-day-input" size='normal'>
+          学習タグ
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={studyTag}
+          onChange={handleTagChange}
+          sx={{ width: '100%' }}
+        >
+          <MenuItem value="">
+            <em>-</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+        <InputLabel sx={{ mt:4 }} shrink htmlFor="study-day-input" size='normal'>
+          学習内容
+        </InputLabel>
+        <TextField 
+          required
+          fullWidth
+          rows={6}
+          value={study}
+          onChange={(value) => setStudy(value.target.value)}
+          multiline
+        />
+      </Box>
+      <Box sx={{ textAlign: 'center', mt: 4 }}>
+        <Fab variant="extended" color="primary" >
+          <SaveIcon sx={{ mr: 1 }} />
+          記録する
+        </Fab>
+      </Box>
+    </Layuot>
+  )
+};
+
+export default Create;
