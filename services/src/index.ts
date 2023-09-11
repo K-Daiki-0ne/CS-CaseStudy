77b7250127ffd91bc4b546e012cb2a5fc6af6966
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-11 10:16:32 +0900

    GraphQL機能をサービスに結合

--- services/src/index.ts
+++ services/src/index.ts
@@ -1,16 +1,49 @@
 import 'reflect-metadata';
-import Express from 'express';
-import { initialize } from './config/data-source';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from 'type-graphql';
+import cors from 'cors';
+import { initialize, AppDataSource } from './config/data-source';
+import { UserResolver, StudyResolver, StudyTagResolver } from './resolvers';
 
 const main = async () => {
-    const app = Express();
 
-    initialize();
+  // expressのバージョンをダウングレード
+  const app = express();
 
-    app.listen(4000, () => console.log('Server listning ...OK'));
+  initialize();
+
+  app.set("trust proxy", 1);
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+
+    }),
+  )
+
+  // type-graphqlはgraphql15.3.0以降のバージョンをサポートしていないため、GraphQLのバージョンは15.3.0とする
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [StudyResolver, UserResolver, StudyTagResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
+
+  
+
+  app.listen(4000, () => console.log('Server listning ...OK'));
 };
 
 main().catch((err) => {
-    console.log('Server listning ...NO');
-    console.error(err);
+  // サーバーの起動に失敗した場合はデータベースとの接続を破棄する
+  AppDataSource.destroy();
+  console.log('Database connection ...NO');
+
+  console.log('Server listning ...NO');
+  console.error(err);
 });
