9fad4c14db45a8d31cfb1b9480a64c2177575cda
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-13 23:27:05 +0900

    create: validate function for form

---
+++ web/src/utils/validateForm.ts
@@ -0,0 +1,39 @@
+type ReturnObject = {
+  field: string,
+  message: string
+}
+
+/**
+ * 
+ * @param email メールアドレス
+ * @param password パスワード
+ * @param mode 使用機能(login or create or register)
+ * @returns ReturnObject or null
+ */
+export const validateForm = (email: string, password: string, mode: string): ReturnObject | null => {
+  if (email == '') {
+    return {
+      field: 'email',
+      message: 'emailが入力されていません'
+    }
+  };
+
+  if (!email.includes('@')) {
+    return {
+      field: 'email',
+      message: '無効なメールアドレスです'
+    }
+  };
+
+  if (mode == 'login') {
+    if (password == '') {
+      return {
+        field: 'password',
+        message: 'emailが入力されていません'
+      }
+    };
+  }
+
+
+  return null;
+}
