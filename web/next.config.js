0638ac6411caad4ce70f5c1baf32cc295bbc960c
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-19 00:53:49 +0900

    recoilのatomエラーを表示しないよう修正

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const intercept = require('next-intercept-stdout');
+const withInterceptStdout = require('next-intercept-stdout');
 
 module.exports = withInterceptStdout(
   {
@@ -9,15 +9,3 @@ module.exports = withInterceptStdout(
   // atomのkeyによるエラーだが、エラーではないためログに表示しない
   (text) => (text.includes('Duplicate atom key') ? '' : text)
 )
-
-// safely ignore recoil warning messages in dev (triggered by HMR)
-function interceptStdout(text) {
-  if (text.includes("Duplicate atom key")) {
-    return ""
-  }
-  return text
-}
-
-if (process.env.NODE_ENV === "development") {
-  intercept(interceptStdout)
-}
