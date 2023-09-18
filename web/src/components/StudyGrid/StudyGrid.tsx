c363669da46a0d1567e35c8a4813c59163045f3a
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-18 15:37:09 +0900

    学習内容の一覧を表示できるよう処理を実装

--- web/src/components/StudyGrid/StudyGrid.tsx
+++ web/src/components/StudyGrid/StudyGrid.tsx
@@ -1,7 +1,13 @@
+import { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import { useLazyQuery } from '@apollo/client';
+import { useRecoilValue } from 'recoil';
+import { MultiReadStudyQuery } from '../../generated/graphql';
+import { MULTI_READ_STUDY } from '../../graphql/graphql'
 import { StudyDeleteButton } from '../StudyDeleteButton/StudyDeleteButton';
-import { StudyEditButton } from '../StudyEditButton/StudyEditButton'
+import { StudyEditButton } from '../StudyEditButton/StudyEditButton';
+import { userInfoState } from '../../store/selectors';
 
 const columns = [
   {
@@ -68,7 +74,7 @@ const columns = [
   }
 ];
 
-const rows = [
+const test = [
   { id: 1, userId: '', Tagid: 1, Study: 'Snow',      Date: '2022年5月12日', Time: '', Content: '' },
   { id: 2, userId: '', Tagid: 2, Study: 'Lannister', Date: 'Cersei', Time: '', Content: '' },
   { id: 3, userId: '', Tagid: 3, Study: 'Lannister', Date: 'Jaime', Time: '', Content: '' },
@@ -80,7 +86,78 @@ const rows = [
   { id: 9, userId: '', Tagid: 9, Study: 'Roxie', Date: 'Harvey', Time: '', Content: '' },
 ];
 
+type StudyArray = {
+  id: number;
+  userId: string;
+  Tagid: number;
+  Study: string;
+  Date: string;
+  Time: string;
+  Content: string
+}
+
 export const StudyGrid = () => {
+  const [rows, setRows] = useState<StudyArray[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const user = useRecoilValue(userInfoState);
+  const [studies] = useLazyQuery<MultiReadStudyQuery>(MULTI_READ_STUDY);
+
+  useEffect(() => {
+    console.log('useEffect - start')
+    const getUserPostStudies = async (): Promise<StudyArray[] | undefined> => {
+      setLoading(true);
+      try {
+        const { data, error } = await studies({
+          variables: {
+            userId: user.userId
+          }
+        })
+
+        if (error) {
+          console.error(error)
+        };
+
+        if (data?.multiReadStudy.studies == undefined) {
+          setLoading(false);
+          return [];
+        }
+
+        const studiesArray: StudyArray[] = [];
+        data.multiReadStudy.studies.map((data) => {
+          const value: StudyArray = {
+            id: data.studyId,
+            userId: data.userId,
+            Tagid: data.tagId,
+            Study: data.Study,
+            Date: data.Date,
+            Time: data.Time,
+            Content: data.Content
+          };
+          studiesArray.push(value);
+        })
+
+        setLoading(false);
+        return studiesArray
+      } catch(e) {
+        console.error(e);
+      }
+    }
+
+    getUserPostStudies()
+      .then((data: StudyArray[] | undefined) => {
+        if (data == undefined) {
+          setRows([...rows])
+        } else {
+          setRows([...rows, ...data])
+        }
+      })
+      .catch((err) => console.error(err));
+
+      console.log('useEffect - end')
+  }, []);
+
+
   return (
     <Box sx={{ height: '100%', width: '100%' }}>
       <DataGrid
@@ -94,8 +171,8 @@ export const StudyGrid = () => {
           },
         }}
         columnVisibilityModel={{ id: false, userId: false, Tagid: false }}
-        pageSizeOptions={[5]}
-        loading={false}
+        // pageSizeOptions={[5]}
+        loading={loading}
         checkboxSelection={false}
         autoHeight
         autoPageSize
