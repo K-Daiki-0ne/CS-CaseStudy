e96499750ca93d4f7e37ab73bbd6da7bdd7f14ca
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-24 23:23:59 +0900

    週間の学習時間と月間の学習時間をグラフで表示できるよう実装

--- web/src/components/StudyChart/StudyChart.tsx
+++ web/src/components/StudyChart/StudyChart.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import {
   Box,
   Typography
@@ -22,48 +23,65 @@ ChartJS.register(
   Legend
 );
 
-const options = {
-  maintainAspectRatio: false,
-  plugins: {
-    title: {
-      display: true,
-      text: '週間の学習時間',
-    },
-  },
-  scales: {
-    x: {
-      stacked: true
-    },
-    y: {
-      stacked: true
-    }
-  },
-};
 
-const labels = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"];
 
-const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: [22.25, 24, 24, 24, 24, 24, 24],
-      backgroundColor: "rgb(255, 99, 132)"
-    },
-    {
-      label: "Dataset 2",
-      data: [1.75, 0, 0, 0, 0, 0, 0],
-      backgroundColor: "rgb(75, 192, 192)"
-    },
-    {
-      label: "Dataset 3",
-      data: [0, 0, 0, 0, 0, 0, 0],
-      backgroundColor: "rgb(53, 162, 235)"
-    }
-  ]
-};
+type WeekChartType = {
+  label: string;
+  data: number[];
+  backgroundColor: string;
+}
 
-export const StudyChart = () => {
+type Props ={
+  props: WeekChartType[]
+}
+
+// const data = {
+//   labels,
+//   datasets: [
+//     {
+//       label: "Dataset 1",
+//       data: [22.25, 24, 24, 24, 24, 24, 24],
+//       backgroundColor: "rgb(255, 99, 132)"
+//     },
+//     {
+//       label: "Dataset 2",
+//       data: [1.75, 0, 0, 0, 0, 0, 0],
+//       backgroundColor: "rgb(75, 192, 192)"
+//     },
+//     {
+//       label: "Dataset 3",
+//       data: [0, 0, 0, 0, 0, 0, 0],
+//       backgroundColor: "rgb(53, 162, 235)"
+//     }
+//   ]
+// };
+
+export const StudyChart: FC<Props> = ({ props }) => {
+
+  const labels = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"];
+
+  const data = {
+    labels,
+    datasets: props
+  }
+  const options = {
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+        display: true,
+        text: '週間の学習時間',
+      },
+    },
+    scales: {
+      x: {
+        stacked: true
+      },
+      y: {
+        stacked: true
+      }
+    },
+  };
+
   return (
     <Box sx={{ flexGrow: 1, border: '1px solid grey'}}>
       <Bar
