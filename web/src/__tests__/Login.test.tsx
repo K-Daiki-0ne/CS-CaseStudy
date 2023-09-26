b226b56a01dfeaede699383617cb112a60e3042c
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-27 00:56:44 +0900

    Login pageのテストコードを作成

---
+++ web/src/__tests__/Login.test.tsx
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render } from '@testing-library/react';
+import Login from '../pages/login';
+import { ApolloProvider } from '@apollo/client';
+import { initializeApollo } from '../libs/apolloClient';
+import { RecoilRoot } from 'recoil';
+import mockRouter from 'next-router-mock';
+
+const apolloClient = initializeApollo();
+
+jest.mock('next/router', () => jest.requireActual('next-router-mock'))
+
+describe('Login page', () => {
+  it('ログイン画面のレンダリングテスト', () => {
+    mockRouter.push('/change-password/1');
+    render(
+      <ApolloProvider client={apolloClient}>
+        <RecoilRoot>
+          <Login  />
+        </RecoilRoot>
+      </ApolloProvider>
+    )
+  })
+})
