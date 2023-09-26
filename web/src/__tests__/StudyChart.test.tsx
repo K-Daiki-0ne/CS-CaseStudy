2e2d51fe06b376f2a58649dd1377d16e3b8c88b0
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-26 19:58:39 +0900

    StudyChart componentのテストファイルを作成

---
+++ web/src/__tests__/StudyChart.test.tsx
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react';
+import { StudyChart } from '../components';
+
+type MockDataType = {
+  label: string;
+  data: number[];
+  backgroundColor: string
+}
+
+const mockData: MockDataType[] = [
+  { label: 'test', data: [0, 0, 0, 0, 0, 0, 0], backgroundColor: '' }
+];
+
+jest.mock('react-chartjs-2', () => ({
+  Bar: () => null,
+}))
+
+describe('StudyChart component', () => {
+  it('StudyChart componentのレンダリングテスト', () => {
+    render(<StudyChart props={mockData} />)
+  });
+})
