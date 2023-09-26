b0d34b10409f35f901988b8b6056ec810d981aba
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-27 00:05:26 +0900

    UserProfile componentのテストコードを作成

---
+++ web/src/__tests__/UserProfile.test.tsx
@@ -0,0 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react';
+import { UserProfile } from '../components';
+import { ApolloProvider } from '@apollo/client';
+import { initializeApollo } from '../libs/apolloClient';
+import userEvent from '@testing-library/user-event';
+import { RecoilRoot } from 'recoil';
+
+const mockUserProfileProps = {
+  userId: 'test',
+  tags: [
+    { key: 1, label: 'test', show: true }
+  ]
+}
+
+const apolloClient = initializeApollo()
+
+describe('UserProfile component', () => {
+  it('UserProfile componentのレンダリングテスト', () => {
+    render(
+      <ApolloProvider client={apolloClient}>
+        <RecoilRoot>
+          <UserProfile userId={mockUserProfileProps.userId} tags={mockUserProfileProps.tags} />
+        </RecoilRoot>
+      </ApolloProvider>
+    )
+  });
+
+  it('UserProfile componentの表示内容テスト', () => {
+    render(
+      <ApolloProvider client={apolloClient}>
+        <RecoilRoot>
+          <UserProfile userId={mockUserProfileProps.userId} tags={mockUserProfileProps.tags} />
+        </RecoilRoot>
+      </ApolloProvider>
+    )
+
+    screen.getByLabelText('ユーザー名')
+    screen.getByLabelText('職業')
+    screen.getByLabelText('学習タグ')
+    screen.getByLabelText('学習目標')
+  });
+
+})
