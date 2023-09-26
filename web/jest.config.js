0dc3ec342424b91ea674939d9117d84a8e86f23c
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-09-26 16:51:19 +0900

    フロントのテストコードを設定

---
+++ web/jest.config.js
@@ -0,0 +1,13 @@
+import nextJest from 'next/jest.js'
+ 
+const createJestConfig = nextJest({
+  dir: './',
+})
+ 
+const config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+ 
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)
