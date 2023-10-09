1b775235c6411643ee9c1a2585b17dd34acf9b8d
_ 0ne <kouguchi3456@yahoo.co.jp>
2023-10-10 00:00:51 +0900

    emailとpasswordを環境変数に変更

--- services/src/utils/sendEmail.ts
+++ services/src/utils/sendEmail.ts
@@ -1,20 +1,19 @@
 import nodemailer from 'nodemailer';
 
-export async function sendEmail(to: string, html: string, subject: string) {
+export async function sendEmail(to: string, html: string) {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    service: 'gmail',
     auth: {
-        user: 'toby61@ethereal.email',
-        pass: 'tSVMq3WW3R4UMfS1SG'
+        user: process.env.CASESTUDY_EMAIL,
+        pass: process.env.CASESTUDY_PASSWORD
     }
 });
 
 // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"CaseStudy" <CaseStudy@gmail.com>',
+    from: '"CaseStudy" <kdaiki.pf.casestudy@gmail.com>',
     to: to,
-    subject: subject, // Subject line
+    subject: "アカウント登録", // Subject line
     html,
   });
 
